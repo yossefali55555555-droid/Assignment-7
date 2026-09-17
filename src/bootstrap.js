695f17a3463944a4d90f67b93673f9064888bfbf
yossef <yossefali55555555@gmail.com>
2026-09-18 00:15:51 +0300

    N

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,8 +7,8 @@ const app = express()
 app.use(express.json())
 export const bootstrap=async()=>{
     await test()
-    app.use("/collection",bookrouter)
-     app.use("/collection",authorrouter)
-     app.use("/collection",logrouter)
+    app.use("/",bookrouter)
+     app.use("/",authorrouter)
+     app.use("/",logrouter)
     app.listen(3000)
 }
